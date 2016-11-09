dd3e2c4b54a7958a13e0f716ee60433d56457e63
Rob Storm <RobStorm@Roberts-MacBook-Pro-8.local>
2016-11-08 20:00:53 -0500

    ticking clock

--- heyo.jsx
+++ heyo.jsx
@@ -1,14 +1,12 @@
     function testo() {
-    	return <h1>sup</h1>;
+    	return <p>sup</p>;
     }
-
-
 
     function getGreeting(user)
     {
       if(user)
       {
-        return <h1>Hey! How's it going {formatName(user)}?</h1>;
+        return <h1>Hey! How is it going {formatName(user)}?</h1>;
       }
       else
       {
@@ -21,6 +19,14 @@
     lastName: 'Stormo'
     };
 
+    const element = (
+    <h1>
+    Hello, {formatName(user)}!
+    </h1>
+    );
+
+    
+
     function formatName(user)
     {
       return user.firstName+' '+user.lastName;
@@ -28,3 +34,18 @@
 
 		const ele = <h1>its rob heyooooo</h1>;
       ReactDOM.render(getGreeting(user), document.getElementById('root'));
+      
+    function tick()
+    {
+
+      const elemento = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+
+      ReactDOM.render(elemento, document.getElementById('poop'));
+    }
+      tick();
+      setInterval(tick, 1000);
