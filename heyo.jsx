b759df31694ec16cf3a16ebfc061800079230caa
Rob Storm <RobStorm@Roberts-MacBook-Pro-8.local>
2016-11-15 17:28:34 -0500

    Got blockos working

--- heyo.jsx
+++ heyo.jsx
@@ -32,8 +32,48 @@
       return user.firstName+' '+user.lastName;
     }
 
+    function Blocko(props)
+    {
+      return(
+        <div>
+          <h1>{props.name}</h1>
+          <p>{props.description}</p>
+        </div>
+        );
+    }
+
+    function Welcome(props)
+    {
+      return <h1>Hello alskjdlaksjdlkajsd {props.name}</h1>;
+    }
+
+    const elee = {name: "Rob Flob", description: "This is the block of rob"};
+    const elee2 = {name: "Crab Tree", description: "I am the crab block"};
+    
+    function showBlock(myBlock)
+    {
+      //return <h1>{myBlock.name}</h1>;
+      return <Blocko name = {myBlock.name} description = {myBlock.description}/>;
+      // return <Welcome name = {myBlock.name}/>;
+    }
+
+    function showBlockos()
+    {
+      return(
+        <div>
+          {showBlock(elee)}
+          {showBlock(elee2)}
+        </div>
+        )
+    }
+
+    function LotsaBlocks()
+    {
+
+    }
+
 		const ele = <h1>its rob heyooooo</h1>;
-      ReactDOM.render(getGreeting(user), document.getElementById('root'));
+      ReactDOM.render(showBlockos(), document.getElementById('root'));
       
     function tick()
     {
